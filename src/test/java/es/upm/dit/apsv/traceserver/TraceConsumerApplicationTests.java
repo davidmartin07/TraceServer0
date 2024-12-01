c49659b86e9c2612c3bc3a019252401e6f2ea4e6
David Martin <david.martin.levy@alumnos.upm.es>
2024-12-01 13:28:21 +0100

    tests

--- src/test/java/es/upm/dit/apsv/traceserver/TraceConsumerApplicationTests.java
+++ src/test/java/es/upm/dit/apsv/traceserver/TraceConsumerApplicationTests.java
@@ -2,14 +2,15 @@ package es.upm.dit.apsv.traceserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+
+
 @SpringBootApplication
-class TraceServerApplicationTests {
+class TraceConsumerApplicationTests {
 
-	@Test
+    @Test
 	void contextLoads() {
 	}
 
 }
+
